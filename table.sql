5381ff74c6a3c2903fcfa71c9eeb860fe4273cf1
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-08-25 09:57:42 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -38,3 +38,46 @@ create or replace table user
         (26, 'ByAdmin-Panel', 'sambit@gmail.com', true, 'Sambit Kumar', 'Male', 'Pradhan', 'default', 'ROLE_ADMIN', 'Tue Aug 23 10:41:26 IST 2022');
 
 
+
+
+
+//Updated
+CREATE TABLE `app` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `date` text NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `regtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(22),
+(22);
+
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `confirmation_token` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `enabled` bit(1) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `authority` varchar(255) DEFAULT NULL,
+  `lastseen` varchar(200) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `user` (`id`, `confirmation_token`, `username`, `enabled`, `first_name`, `gender`, `last_name`, `password`, `authority`, `lastseen`) VALUES
+(1, '36983cce-975b-4a92-bf73-a4f41978e01c', 'sambit@gmail.com', b'1', 'Sambit', 'MALE', 'Pradhan', 'sambitKumar', 'ROLE_ADMIN', 'Thu Aug 22 00:00:56 IST 2022');
+
